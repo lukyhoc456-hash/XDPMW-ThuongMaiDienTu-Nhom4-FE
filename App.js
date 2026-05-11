af5982a0118832fad7849d811f9f1a299aa65d58
Learncode2004 <lukyhoc789@gmail.com>
2026-05-11 16:31:38 +0700

    FE

--- App.js
+++
@@ -1,8 +0,0 @@
-function App() {
-      return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-          <h1>Hello World from React!</h1>
-        </div>
-      );
-    }
-    export default App;
